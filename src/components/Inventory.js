2cbcb97c0b894215cfc05a7783f05192a68c4f7d
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-03-18 21:48:34 -0600

    :sparkles: Creating application layout with components

---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Inventory extends Component {
+  render() {
+    return <div className="Inventory">Inventory</div>;
+  }
+}
+
+export default Inventory;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { Component} from "react";
 
-class StorePicker extends React.Component {
+class StorePicker extends Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
         <input type="text" required placeholder="Store Name" />
-        <button type="submit">Visit Store</button>
+        <button type="submit">Visit Store :rocket:</button>
       </form>
     );
   }
