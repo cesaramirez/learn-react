94b5f77e707d8e8eea9d437815aef1e43e7aabca
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-03-23 17:20:09 -0600

    :sparkles: Add state to fish form

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,18 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+
+    this.setState({
+      fishes
+    });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +23,7 @@ class App extends Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends Component {
   render() {
-    return <div className="Inventory">Inventory</div>;
+    return (
+      <div className="Inventory">
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
   }
 }
 
