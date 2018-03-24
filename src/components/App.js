ba4a72a83b8d1f24d7783c79ec9e0d2cf6d51ea9
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-03-23 22:34:50 -0600

    :sparkles: Update order state

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,12 @@ class App extends Component {
   loadSampleFishes = () => {
     this.setState({ fishes: SampleFishes });
   };
+
+  addToOrder = key => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -28,7 +34,12 @@ class App extends Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
